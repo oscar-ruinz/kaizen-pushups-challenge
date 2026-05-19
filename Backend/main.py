b8de0dbf0c196d515f398da0e5b348ca11ef92ca
Oscar Ruelas <oscar.ruelasinz1999@gmail.com>
2026-05-18 21:35:50 -0700

    fix CORS

--- Backend/main.py
+++ Backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Push-ups Challenge API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
